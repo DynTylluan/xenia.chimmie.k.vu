50057fc19a3da462970f321f8446d0765c253346
GamePlayer-8 <gameplayer2019pl@tutamail.com>
2024-01-17 21:34:11 +0100

    Fix description alignment.

--- res/celement.sh
+++ res/celement.sh
@@ -48,7 +48,8 @@ echo 'Configuring "'"${TARGET}.png.c.html"'"...'
 sh "$SCRIPT_PATH/scripts/set.sh" "$OUT_PATH/${TARGET}.png.c.conf" "$OUT_PATH/${TARGET}.png.c.html"
 
 if [ -f "$SRC_PATH/../desc/${TARGET}.md" ]; then
-	sed -i "s/%DESCRIPTION%/$(markdown "$SRC_PATH/../desc/${TARGET}.md")/g" "$OUT_PATH/${TARGET}.png.c.html"
+	sed -i -e '/%DESCRIPTION%/e markdown '"$SRC_PATH/../desc/${TARGET}.md" "$OUT_PATH/${TARGET}.png.c.html"
+	sed -i 's/%DESCRIPTION%//g' "$OUT_PATH/${TARGET}.png.c.html"
 else
 	sed -i 's/%DESCRIPTION%/No description provided./g' "$OUT_PATH/${TARGET}.png.c.html"
 fi
